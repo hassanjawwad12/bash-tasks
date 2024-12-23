aecd63fa8654b2bd9bd78d45f453c42a6998204f
Hassan Jawwad <hassan.jawwad@datumbrain.com>
2024-12-23 18:32:38 +0500

    Rectified the time error

--- task4.sh
+++ task4.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
-<<"COMMENT" 
-Modify your PS1 and make it look like
-[15:52:13] ~ >
-Where [15:52:13] is timestamp, ~ is your present working directory.
-Put this in ~/.dotfile and execute this dot file safely (only if present) through ~/.bashrc 
-COMMENT 
+# Modify your PS1 and make it look like
+# [15:52:13] ~ >
+# Where [15:52:13] is timestamp, ~ is your present working directory.
+# Put this in ~/.dotfile and execute this dot file safely (only if present) through ~/.bashrc 
 
-MODIFIED=PS1="[\$(date +%T)] \w > " 
-PRESENT="$HOME/.dotfile"
+#we write the prompt to the dot file
+echo "PS1='[\$(date +%T)] \w > '" > ~/.dotfile
 
-#writing content to file 
-cat > "PRESENT" << EOF 
-PS1='[\$(date +%T)] \w > ' #saved the format as HH:mm:ss
-EOF
+#we append sourcing to bashrc
+echo "source ~/.dotfile" >> ~/.bashrc
 
-SOURCED="$HOME/.bashrc" 
+echo "Run source ~/.bashrc on the teriminal to see the changes"
 
-if grep -q "source ~/.dotfile" "$SOURCED"; then 
-    echo "Dotfile is already sourced"
-else
-    echo "DOTFILE Added"
-fi
-echo "Run source ~/.bashrc or restart the terminal"
+source ~/.bashrc
